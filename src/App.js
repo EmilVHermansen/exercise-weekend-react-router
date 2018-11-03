a1fcfbe5766a259444134ae6efcfb71cb137c085
emil.valbak.hermansen@gmail.com <emil.valbak.hermansen@gmail.com>
2018-11-03 17:28:13 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,106 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+function BasicExample() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">About</Link>
+          </li>
+          <li>
+            <Link to="/about-react">About React</Link>
+          </li>
+          <li>
+            <Link to="/jsx">JSX</Link>
+          </li>
+          <li>
+            <Link to="/props-and-state">Props & state</Link>
+          </li>
+          <li>
+            <Link to="/lifecycle-hooks">Lifecycle hooks</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={AboutMe} />
+        <Route path="/about-react" component={AboutReact} />
+        <Route path="/JSX" component={jsx} />
+        <Route path="/props-and-state" component={PropsAndState} />
+        <Route path="/lifecycle-hooks" component={LifecycleHooks} />
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
-export default App;
+function AboutMe() {
+  return (
+    <div>
+      <h2>About me</h2>
+      <h3>Emil Valbak Hermansen</h3>
+      <p></p>
+    </div>
+  );
+}
+
+function AboutReact() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function jsx() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function PropsAndState() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function LifecycleHooks({ match }) {
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      <Route path={`${match.path}/:topicId`} component={Topic} />
+      <Route
+        exact
+        path={match.path}
+        render={() => <h3>Please select a topic.</h3>}
+      />
+    </div>
+  );
+}
+
+function Topic({ match }) {
+  return (
+    <div>
+      <h3>{match.params.topicId}</h3>
+    </div>
+  );
+}
+
+export default BasicExample;
