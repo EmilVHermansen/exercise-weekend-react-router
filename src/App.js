cff81ce0e05a45ad09971ed7d631edebbbcbfdad
emil.valbak.hermansen@gmail.com <emil.valbak.hermansen@gmail.com>
2018-11-03 18:05:40 +0100

    Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link }  from "react-router-dom";
 
 function BasicExample() {
   return (
@@ -7,7 +7,7 @@ function BasicExample() {
       <div>
         <ul>
           <li>
-            <Link to="/">About</Link>
+            <Link to="/">About me</Link>
           </li>
           <li>
             <Link to="/about-react">About React</Link>
@@ -40,7 +40,7 @@ function AboutMe() {
     <div>
       <h2>About me</h2>
       <h3>Emil Valbak Hermansen</h3>
-      <p></p>
+      <a href="https://github.com/EmilVHermansen/exercise-weekend-react-router">Link to my github</a>
     </div>
   );
 }
@@ -48,7 +48,8 @@ function AboutMe() {
 function AboutReact() {
   return (
     <div>
-      <h2>About</h2>
+      <h2>About React</h2>
+      <p>React is an library used for transferring data to a UI. It consists of class and function based components. </p>
     </div>
   );
 }
@@ -56,7 +57,8 @@ function AboutReact() {
 function jsx() {
   return (
     <div>
-      <h2>About</h2>
+      <h2>What is JSX</h2>
+      <p>JSX is the syntax used in React where you can mix HTML formatting and javascript, much like JSP for Java </p>
     </div>
   );
 }
@@ -64,7 +66,11 @@ function jsx() {
 function PropsAndState() {
   return (
     <div>
-      <h2>About</h2>
+      <h2>Props and state</h2>
+      <ul>
+        <li>Props is immutable variables given throught parameters in a function </li>
+        <li>State is a mutable variable set in components, and by default executes the render function, whenever state is updated </li>
+      </ul>
     </div>
   );
 }
@@ -75,13 +81,22 @@ function LifecycleHooks({ match }) {
       <h2>Topics</h2>
       <ul>
         <li>
-          <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+          <Link to={`${match.url}/componentDidMount`}>componentDidMount()</Link>
         </li>
         <li>
-          <Link to={`${match.url}/components`}>Components</Link>
+          <Link to={`${match.url}/render`}>render()</Link>
         </li>
         <li>
-          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+          <Link to={`${match.url}/componentWillReceiveProps`}>UNSAFE_componentWillReceiveProps(nextProps)</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/shouldComponentUpdate`}>shouldComponentUpdate(nextProps, nextState)</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/componentWillUpdate`}>UNSAFE_componentWillUpdate(nextProps, nextState)</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/componentWIllUnmount`}>componentWIllUnmount()</Link>
         </li>
       </ul>
 
@@ -96,11 +111,56 @@ function LifecycleHooks({ match }) {
 }
 
 function Topic({ match }) {
+  let topicText = "";
+  
+  switch(match.params.topicId){
+    case "componentDidMount":
+    {
+      topicText ="componentDidMount() is invoked immediately after a component is mounted (inserted into the tree)." +
+      "Initialization that requires DOM nodes should go here. If you need to load data from a remote endpoint, "+
+      "this is a good place to instantiate the network request."
+      break;
+    }
+    case "render":
+    {
+      topicText ="The render() method is the only required method in a class component. "+
+      "When called, it should examine this.props and this.state and return a react component or HTML formats"
+      break;
+    }
+    case "componentWillReceiveProps":
+    {
+      topicText ="UNSAFE_componentWillReceiveProps() is invoked before a mounted component receives new props. If you need to update the state in response to prop changes (for example, to reset it), you may compare this.props and nextProps and perform state transitions using this.setState() in this method."
+      break;
+    }
+    case "shouldComponentUpdate":
+    {
+      topicText ="Use shouldComponentUpdate() to let React know if a component’s output is not affected by the current change in state or props. The default behavior is to re-render on every state change, and in the vast majority of cases you should rely on the default behavior."
+      break;
+    }
+    case "componentWillUpdate":
+    {
+      topicText ="UNSAFE_componentWillUpdate() is invoked just before rendering when new props or state are being received. Use this as an opportunity to perform preparation before an update occurs. This method is not called for the initial render."
+      break;
+    }
+    case "componentWIllUnmount":
+    {
+      topicText ="componentWillUnmount() is invoked immediately before a component is unmounted and destroyed. Perform any necessary cleanup in this method, such as invalidating timers, canceling network requests, or cleaning up any subscriptions that were created in componentDidMount()."
+      break;
+    }
+    default:
+    {
+      topicText = "Åh nej default"
+      break;
+    }
+  }
+
   return (
     <div>
       <h3>{match.params.topicId}</h3>
+      <p>{topicText}</p>
     </div>
   );
 }
+
 
 export default BasicExample;
